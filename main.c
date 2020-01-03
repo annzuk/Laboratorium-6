c2b815ca9c236dcee487f7997a48cb0bafd44baf
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-01-03 18:49:05 +0100

    Third commit by Wojtek

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 int main()
 {
-    printf("Hello World");
+    printf("-----MENU------");
+    printf("1. Losowa liczba");
+    printf("2. Wyświetlenie liczb od 1 do 10");
+    printf("3. Wyświetlenie Twojego imienia");
     return 0;
 }
